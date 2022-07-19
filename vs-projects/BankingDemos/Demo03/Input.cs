b28da86046ab43a5ec6f0663ab86e292428d515b
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2022-07-19 14:05:50 +0530

    Day#5 update#1

---
+++ vs-projects/BankingDemos/Demo03/Input.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.Utils
+{
+    public class Input
+    {
+        public string ReadString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public double ReadDouble(string prompt)
+        {
+            return double.Parse(ReadString(prompt));
+        }
+
+        public int ReadInt(string prompt)
+        {
+            return int.Parse(ReadString(prompt));   
+        }
+            
+
+    }
+}
